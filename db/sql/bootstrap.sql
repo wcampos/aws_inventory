c9cde6f7ce7c35b522e5446bddcda8aa75c97dff
wcampos <wil@valuestreamengineers.com>
2022-07-03 23:49:28 -0500

    Adding DB files

--- db/sql/bootstrap.sql
+++ db/sql/bootstrap.sql
@@ -1,8 +1,13 @@
+USE inventory;
+
 CREATE TABLE profiles(
-id MEDIUMINT NOT NULL AUTO_INCREMENT,
+id SERIAL,
 creation_date TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
 name VARCHAR(255) NOT NULL,
 access_key VARCHAR(255) NOT NULL,
 secret_key VARCHAR(255) NOT NULL,
 PRIMARY KEY(id)
 );
+
+
+select * from profiles;
