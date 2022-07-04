fbd6e712a312a2e2a5cdff51800629ce8d00c078
wcampos <wil@valuestreamengineers.com>
2022-07-03 23:41:16 -0500

    Adding profile db functions

---
+++ src/db_classes.py
@@ -0,0 +1,57 @@
+import mysql.connector
+import configparser
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+conn = mysql.connector.connect(
+    host=config['DB_CONFIG']['db_host'],
+    port=3306,
+    user=config['DB_CONFIG']['db_user'],
+    password=config['DB_CONFIG']['db_password'],
+    database=config['DB_CONFIG']['db_name']
+)
+
+
+def get_profiles():
+    sql_statement = "select name from inventory.profiles"
+
+    try:
+        cursor = conn.cursor()
+        cursor.execute(sql_statement)
+        results = [''.join(q) for q in cursor.fetchall()]
+    except mysql.connector.Error as err:
+        results = "Something went wrong! Refer to: {}".format(err)
+    conn.close()
+    return results
+
+
+def add_profile(in_name, in_access, in_secret):
+    sql_statement = ("INSERT INTO profiles "
+                     "(name, access_key, secret_key) "
+                     "VALUES (%(name)s, %(access_key)s, %(secret_key)s)")
+
+    data_profile = {
+        'name': in_name,
+        'access_key': in_access,
+        'secret_key': in_secret,
+    }
+
+    # if get_profiles().count(in_name) > 0:
+    #    print("A profile with name: " + in_name + " already exists!")
+    #    exit(1)
+
+    try:
+        cursor = conn.cursor()
+        cursor.execute(sql_statement, data_profile)
+        conn.commit()
+        return_msg = "The profile " + in_name + " was added to the DB!"
+    except mysql.connector.Error as err:
+        return_msg = "Something went wrong! Refer to: {}".format(err)
+
+    conn.close()
+    print(return_msg)
+
+# Test
+# print(get_profiles())
+# add_profile("vse-dtw", "65d6a5da5d65a", "dahdhakdkasdas4d4sd4sd")
